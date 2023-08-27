6a065dde839c3db0314579452c4f12f623bc5c87
AaronARdz <aaron_acostarz@outlook.es>
2023-08-27 16:23:22 -0600

    initial commit, docker production/dev build test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to docker tutorial
         </a>
       </header>
     </div>
